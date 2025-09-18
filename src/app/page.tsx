7b48185334d73679dee99a93bb50fc6b387b6b7e
Sanmi Akinwunmi <sanmi.hq@gmail.com>
2025-09-18 02:17:27 +0100

    add: shadcn-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center">Hello world</div>
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1>Hello world</h1>
+      <Button>
+        <Link href="/login">Get Started - It's Free</Link>
+      </Button>
+    </div>
   );
 }
