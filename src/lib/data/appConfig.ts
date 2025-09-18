dbf9ad8b75c90bbfad213be5a9f4e572d76a4313
Sanmi Akinwunmi <sanmi.hq@gmail.com>
2025-09-18 01:08:28 +0100

    chore: setup fonts and global styles

---
+++ src/lib/data/appConfig.ts
@@ -0,0 +1,23 @@
+export const appConfig = {
+  name: "Flowforms",
+  title: "Flowforms: Build forms that flow beautifully.",
+  description:
+    "Effortlessly create and manage beautiful forms with our minimalist, production-ready form builder.",
+  links: {
+    home: "/",
+    features: "/#features",
+    docs: "/docs",
+    login: "/login",
+    dashboard: "/dashboard",
+  },
+  developer: {
+    name: "Sanmi Akinwunmi",
+    links: {
+      github: "https://github.com/sanmihq",
+      twitter: "https://x.com/sanmi_hq",
+      linkedin: "https://www.linkedin.com/in/sanmihq/",
+    },
+  },
+};
+
+export type AppConfig = typeof appConfig;
